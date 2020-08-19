1ef90ce96be72e65d4509d51585707f5a344ca6a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2020-08-19 02:51:46 -0500

    fixed bug where link url would open another page when one did not exist

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,7 +10,7 @@ export default class Porfolio extends Component {
             name: "test",
             description: "test",
             longDescription: [],
-			linkUrl: "test",
+            linkUrl: "test",
         };
     }
 
@@ -19,78 +19,82 @@ export default class Porfolio extends Component {
             name,
             description,
             longDescription,
-			linkUrl,
+            linkUrl,
         } = params;
         this.setState({
             name,
             description,
             longDescription,
-			linkUrl
+            linkUrl
         });
     }
 
     render() {
         let resumeData = this.props.resumeData;
         return (
-          <section id="portfolio">
-            <div className="row">
-              <div className="twelve columns collapsed">
-                <h1> Check Out Some of My Works. </h1>
-                <div
-                  id="portfolio-wrapper"
-                  className="bgrid-quarters s-bgrid-thirds cf"
-                >
-                  {resumeData.portfolio &&
-                    resumeData.portfolio.map((item, index) => {
-                      return (
-                        <div className="columns portfolio-item" key={index}>
-                          <div className="item-wrap">
-                            <a
-                              href="#portfolioModal"
-                              onClick={() => this.changeModal(item)}
-                            >
-                              <img
-                                src={`${item.imgUrl}`}
-                                className="item-img"
-                              />
-                              <div className="overlay">
-                                <div className="portfolio-item-meta">
-                                  <h5> {item.name} </h5>
-                                  <p> {item.description} </p>
-                                </div>
-                              </div>
-                            </a>
-                          </div>
+            <section id="portfolio">
+                <div className="row">
+                    <div className="twelve columns collapsed">
+                        <h1> Check Out Some of My Works. </h1>
+                        <div
+                            id="portfolio-wrapper"
+                            className="bgrid-quarters s-bgrid-thirds cf"
+                        >
+                            {resumeData.portfolio &&
+                                resumeData.portfolio.map((item, index) => {
+                                    return (
+                                        <div className="columns portfolio-item" key={index}>
+                                            <div className="item-wrap">
+                                                <a
+                                                    href="#portfolioModal"
+                                                    onClick={() => this.changeModal(item)}
+                                                >
+                                                    <img
+                                                        src={`${item.imgUrl}`}
+                                                        className="item-img"
+                                                    />
+                                                    <div className="overlay">
+                                                        <div className="portfolio-item-meta">
+                                                            <h5> {item.name} </h5>
+                                                            <p> {item.description} </p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
                         </div>
-                      );
-                    })}
+                    </div>
                 </div>
-              </div>
-            </div>
-            <div id="portfolioModal" className="popup-modal mfp-hide">
-              <i className="fa fa-times close" aria-hidden="true"></i>
-              <div className="description-box">
-                <a
-                  href={this.state.linkUrl ? this.state.linkUrl : "#portfolio"}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <h4> {this.state.name} </h4>
-                </a>
-                <p> {this.state.description} </p>
-                <ul className="description-list dashed">
-                  {this.state.longDescription &&
-                    this.state.longDescription.map((desc, index) => {
-                      return (
-                        <li key={index}>
-                          <em> {desc} </em>
-                        </li>
-                      );
-                    })}
-                </ul>
-              </div>
-            </div>
-          </section>
+                <div id="portfolioModal" className="popup-modal mfp-hide">
+                    <i className="fa fa-times close" aria-hidden="true"></i>
+                    <div className="description-box">
+                        {
+                            this.state.linkUrl ? (
+                                <a
+                                    href={this.state.linkUrl}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <h4> {this.state.name} </h4>
+                                </a>
+                            ) : null
+                        }
+                        <p> {this.state.description} </p>
+                        <ul className="description-list dashed">
+                            {this.state.longDescription &&
+                                this.state.longDescription.map((desc, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <em> {desc} </em>
+                                        </li>
+                                    );
+                                })}
+                        </ul>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
